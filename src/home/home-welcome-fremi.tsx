d327fcf04062d5e6fe86d3709a24505910007f27
Apollo <apollomacabodbod8@gmail.com>
2026-02-19 20:52:31 +0800

    added home welcom fremi animation

--- src/home/home-welcome-fremi.tsx
+++ src/home/home-welcome-fremi.tsx
@@ -1,12 +1,40 @@
+import { motion } from "framer-motion";
+
 export default function HomeWelcomeFremi() {
   return (
     <>
       <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] mt-[3em] gap-[1em]">
-        <p className="text-[1.625rem] text-[#2b2b2b] font-openSans font-bold text-center">
+        <motion.p
+          className="text-[1.625rem] text-[#2b2b2b] font-openSans font-bold text-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Velkommen til Fremi!
-        </p>
+        </motion.p>
 
-        <div className="flex items-center justify-center">
+        <motion.div
+          className="flex items-center justify-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           <svg
             width="60"
             height="4"
@@ -15,7 +43,7 @@ export default function HomeWelcomeFremi() {
           >
             <rect width="60" height="4" rx="2" fill="#5ab732" />
           </svg>
-        </div>
+        </motion.div>
       </div>
     </>
   );
