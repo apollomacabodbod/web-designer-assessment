171a816291bc8920debe588a77a1f1aa9a6b878e
Apollo <apollomacabodbod8@gmail.com>
2026-02-19 14:01:15 +0800

    added home transport

---
+++ src/home/home-transport.tsx
@@ -0,0 +1,22 @@
+export default function HomeTransport() {
+  return (
+    <>
+      <div className="flex flex-col mt-[2em] max-w-[1200px] lg:mx-auto  px-[1.25em] gap-[1.5em] ">
+        <p className="text-center text-[#2b2b2b] font-openSans font-bold text-[1.625rem]">
+          Graving, drenering og transport av masser i Møre og Romsdal!
+        </p>
+
+        <div className="flex items-center justify-center">
+          <svg
+            width="60"
+            height="4"
+            viewBox="0 0 60 4"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="60" height="4" rx="2" fill="#5ab732" />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+}
