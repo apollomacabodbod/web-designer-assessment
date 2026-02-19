214f9d506cea0e0d8386807fe56d17cabc0c480f
Apollo <apollomacabodbod8@gmail.com>
2026-02-19 20:58:42 +0800

    added home fremi description animation

--- src/home/home-fremi-description.tsx
+++ src/home/home-fremi-description.tsx
@@ -1,8 +1,23 @@
+import { motion } from "framer-motion";
+
 export default function HomeFremiDescription() {
   return (
     <>
       <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] mt-[3em] gap-[1.5em]">
-        <p className="font-openSans text-[0.9375rem] font-normal text-[#555]">
+        <motion.p
+          className="font-openSans text-[0.9375rem] font-normal text-[#555]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           <span className="text-[#555] text-[0.9375rem] font-bold font-openSans">
             Fremi{" "}
           </span>
@@ -13,11 +28,24 @@ export default function HomeFremiDescription() {
           utført profesjonelt og med svært høy standard. Materialer som anvendes
           er av høy kvalitet fra kjente vareleverandører. Vår styrke er er
           tillit og fokus på beste løsninger samt alltid topp kvalitet.
-        </p>
+        </motion.p>
 
-        <p className="text-[#555] font-openSans text-[0.9375rem] font-bold ">
+        <motion.p
+          className="text-[#555] font-openSans text-[0.9375rem] font-bold "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Kontakt oss for en hyggelig og uforpliktende prat!
-        </p>
+        </motion.p>
       </div>
     </>
   );
